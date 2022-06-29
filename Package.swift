0ec5638168900e0abd99d2881eb676db598ca0a0
Morten Just <morten.just@genstart.dk>
2022-06-29 21:45:04 +0200

    Enable macOS

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ import PackageDescription
 
 let package = Package(
     name: "AsyncCompatibilityKit",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "AsyncCompatibilityKit",
--- Sources/Publisher+Async.swift
+++ Sources/Publisher+Async.swift
@@ -7,6 +7,7 @@
 import Combine
 
 @available(iOS, deprecated: 15.0, message: "AsyncCompatibilityKit is only useful when targeting iOS versions earlier than 15")
+@available(macOS, deprecated: 12.0, message: "AsyncCompatibilityKit is only useful when targeting macOS versions earlier than 12.0")
 public extension Publisher {
     /// Convert this publisher into an `AsyncThrowingStream` that
     /// can be iterated over asynchronously using `for try await`.
